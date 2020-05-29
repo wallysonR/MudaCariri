acfa8da6d4a4f7f08644534f2615da323b6242f3
Wallyson <wallyson@aluno.fapce.edu.br>
2020-05-29 18:31:18 -0300

    :construction: melhorando navegação

--- project/app/forms.py
+++ project/app/forms.py
@@ -3,13 +3,12 @@ from .models import *
 from django.contrib.auth.models import User
 
 
-
 class PerfilForm(forms.ModelForm):
     rua = forms.CharField(widget=forms.TextInput(
-        attrs={'class':'form-control','name':'rua'}), label = "Rua")
-    cidade = forms.Select(attrs={'class':'form-control','name':'cidade'})
+        attrs={'class': 'form-control', 'name': 'rua'}), label="Rua")
+    cidade = forms.Select(attrs={'class': 'form-control', 'name': 'cidade'})
 
     class Meta:
         model = Perfil
-        fields = ['rua','cidade']
+        fields = ['rua', 'cidade']
 
--- project/app/views.py
+++ project/app/views.py
@@ -5,39 +5,31 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
-from django.shortcuts import render, redirect,get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.forms import ModelForm
-from .models import Perfil
-from .forms import *
+from .models import Perfil, Anuncio
+from .forms import Perfil
 
-# todo Gerenciador de mensagens try catch
-
-# FORMS
 
 class PerfilForm(ModelForm):
     class Meta:
         model = Perfil
-        fields = ['rua','cidade']
+        fields = ['rua', 'cidade']
+
 
 class AnuncioForm(ModelForm):
     class Meta:
         model = Anuncio
-        fields = ['nome','descricao']
-
-
-#HOME
+        fields = ['nome', 'descricao']
 
 
 def home(request, template_name='home.html'):
     return render(request, template_name)
 
 
-#USUARIO
-
-
-def registrar_usuario(request,template_name="form_usuario.html"):
+def registrar_usuario(request, template_name="form_usuario.html"):
     form = PerfilForm(request.POST or None)
-    if request.method=="POST":
+    if request.method == "POST":
         name = request.POST['username']
         email = request.POST['email']
         password = request.POST['password']
@@ -45,32 +37,22 @@ def registrar_usuario(request,template_name="form_usuario.html"):
         cidade_requisicao = "juazeiro"
         user = User.objects.create_user(name, email, password)
         user.save()
-        perfil = Perfil.objects.create(rua=rua_requesicao,cidade=cidade_requisicao,usuario=user)
+        perfil = Perfil.objects.create(
+            rua=rua_requesicao, cidade=cidade_requisicao, usuario=user)
         perfil.save()
         return redirect('/login')
-    return render(request, template_name, {'form':form})
+    return render(request, template_name, {'form': form})
 
-# TODO EDITAR_USUARIO(PERFIL),EXCLUIR_USUARIO(PERFIL) 
-# def editar_usuario(request,pk,template_name="form_usuario.html"):
-#     perfil = get_object_or_404(Perfil,pk=pk)
-#     usuario = get_object_or_404(User, pk = perfil.usuario_id)
-#     if usuario.is_activate:
-#         if request.method_is == "POST":
-#             form_perfil = PerfilForm(request.POST, instance=perfil)
-#             form_usuario = 
-#             if form.is_valid():
-#                 form.save()
-                
-def deletar_usuario(request,pk, template_name="delete.html"):
-    perfil = get_object_or_404(Perfil,pk = pk)
-    usuario = get_object_or_404(User, id = perfil.usuario_id)
+
+def deletar_usuario(request, pk, template_name="delete.html"):
+    perfil = get_object_or_404(Perfil, pk=pk)
+    usuario = get_object_or_404(User, id=perfil.usuario_id)
     if request.method == "POST":
         usuario.is_active = False
         usuario.save()
         return HttpResponse("Usuario deletado com sucesso!")
     else:
-        return render(request,template_name,{'usuario':usuario})
-
+        return render(request, template_name, {'usuario': usuario})
 
 
 def logar_usuario(request, template_name='login.html'):
@@ -86,39 +68,39 @@ def logar_usuario(request, template_name='login.html'):
             return HttpResponseRedirect(settings.LOGIN_URL)
     return render(request, template_name, {'redirect_to': home})
 
+
 def deslogar(request):
     logout(request)
     return redirect(home)
 
- 
- 
- # ANUNCIO
-
 
 @login_required
-def registrar_anuncio(request,template_name="form_anuncio.html"):
+def registrar_anuncio(request, template_name="form_anuncio.html"):
     form = AnuncioForm(request.POST or None)
-    if request.method=="POST":
+    if request.method == "POST":
         nome_planta = request.POST['nome']
         descricao_planta = request.POST['descricao']
-        usuario1 = Perfil.objects.get(usuario_id = request.user.id)
-        anuncio = Anuncio.objects.create(nome=nome_planta,descricao=descricao_planta,perfil=usuario1)
+        usuario1 = Perfil.objects.get(usuario_id=request.user.id)
+        anuncio = Anuncio.objects.create(
+            nome=nome_planta, descricao=descricao_planta, perfil=usuario1)
         anuncio.save()
         return redirect('listar_anuncio')
-    return render(request,template_name,{'form':form})
+    return render(request, template_name, {'form': form})
+
 
 @login_required
 def listar_anuncio(request, template_name="listar_anuncio.html"):
-    anuncios = Anuncio.objects.filter(ativo = True)
-    anuncio = {'lista':anuncios}
-    return render(request,template_name,anuncio)      
+    anuncios = Anuncio.objects.filter(ativo=True)
+    anuncio = {'lista': anuncios}
+    return render(request, template_name, anuncio)
+
 
 @login_required
-def editar_anuncio(request,pk ,template_name='form_anuncio.html'):
+def editar_anuncio(request, pk, template_name='form_anuncio.html'):
     anuncio = get_object_or_404(Anuncio, pk=pk)
     if anuncio.ativo == False:
-        #TODO mudar para uma subclasse do HTTPRESPONSE 
-        return HttpResponse ('Esse anuncio está desativado')
+        # TODO mudar para uma subclasse do HTTPRESPONSE
+        return HttpResponse('Esse anuncio está desativado')
     else:
         if request.method == "POST":
             form = AnuncioForm(request.POST, instance=anuncio)
@@ -127,21 +109,14 @@ def editar_anuncio(request,pk ,template_name='form_anuncio.html'):
                 return redirect('listar_anuncio')
         else:
             form = AnuncioForm(instance=anuncio)
-    return render(request,template_name,{'form': form})
+    return render(request, template_name, {'form': form})
+
 
 @login_required
-def deletar_anuncio(request,pk, template_name='delete_anuncio.html'):
+def deletar_anuncio(request, pk, template_name='delete_anuncio.html'):
     anuncio = get_object_or_404(Anuncio, pk=pk)
     if request.method == "POST":
         anuncio.ativo = False
         anuncio.save()
         return redirect('listar_anuncio')
-    return render(request,template_name,{'anuncio':anuncio})
-
-
-
-
-
-
-
-
+    return render(request, template_name, {'anuncio': anuncio})
